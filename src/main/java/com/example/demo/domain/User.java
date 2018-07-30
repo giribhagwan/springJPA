ada44d43b1446fcb76423da20200e4f923c74036
Bhgwan <melayer.bhagwan@gmail.com>
2018-07-30 13:09:35 +0530

    add user id generated strategy

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -2,12 +2,13 @@ package com.example.demo.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer sId;
     private String fullName;
     private String address;
